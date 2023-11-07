d7f9e64be86d07b65e40d63803ee5657281e3575
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-11-06 22:00:35 -0800

    moved mongodb code to model

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,3 @@
-import static com.mongodb.client.model.Filters.eq;
 
 import org.bson.Document;
 
@@ -7,22 +6,48 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class App {
-    public static void main( String[] args ) {
 
-        // Replace the placeholder with your MongoDB deployment's connection string
-        String uri = "mongodb://cshukla:dojacat123@ac-xi6azbp-shard-00-00.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-01.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-02.uw5iowu.mongodb.net:27017/?ssl=true&replicaSet=atlas-bkvh70-shard-0&authSource=admin&retryWrites=true&w=majority";
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //AppFrame root = new AppFrame(primaryStage);                       // instantiating the entire appframe 
 
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
-            if (doc != null) {
-                System.out.println(doc.toJson());
-            } else {
-                System.out.println("No matching documents found.");
-            }
-        }
+        /**
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        **/
+        Model model = new Model();
+        View view = new View(primaryStage);
+        Controller controller = new Controller(view, model);
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
+
+
+// public class App {
+//     public static void main( String[] args ) {
+
+//         // Replace the placeholder with your MongoDB deployment's connection string
+//         String uri = "mongodb://cshukla:dojacat123@ac-xi6azbp-shard-00-00.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-01.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-02.uw5iowu.mongodb.net:27017/?ssl=true&replicaSet=atlas-bkvh70-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+//         try (MongoClient mongoClient = MongoClients.create(uri)) {
+//             MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+//             MongoCollection<Document> collection = database.getCollection("movies");
+
+//             Document doc = collection.find(eq("title", "Back to the Future")).first();
+//             if (doc != null) {
+//                 System.out.println(doc.toJson());
+//             } else {
+//                 System.out.println("No matching documents found.");
+//             }
+//         }
+//     }
+// }
--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,7 +1,26 @@
+import static com.mongodb.client.model.Filters.eq;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Model{
     public Model(){
+        String uri = "mongodb://cshukla:dojacat123@ac-xi6azbp-shard-00-00.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-01.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-02.uw5iowu.mongodb.net:27017/?ssl=true&replicaSet=atlas-bkvh70-shard-0&authSource=admin&retryWrites=true&w=majority";
 
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 }
