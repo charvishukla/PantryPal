d42068d6000b0a8e75a97db6bd633902e7200f61
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-11-06 22:14:02 -0800

    added new MongoDB URI

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -9,18 +9,10 @@ import com.mongodb.client.MongoDatabase;
 
 public class Model{
     public Model(){
-        String uri = "mongodb://cshukla:dojacat123@ac-xi6azbp-shard-00-00.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-01.uw5iowu.mongodb.net:27017,ac-xi6azbp-shard-00-02.uw5iowu.mongodb.net:27017/?ssl=true&replicaSet=atlas-bkvh70-shard-0&authSource=admin&retryWrites=true&w=majority";
+        String uri = "mongodb+srv://team13:CohNSwNGemiYmOOI@cluster0.1nejphw.mongodb.net/?retryWrites=true&w=majority";
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
-
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
-            if (doc != null) {
-                System.out.println(doc.toJson());
-            } else {
-                System.out.println("No matching documents found.");
-            }
+            System.out.println("Successfully connected to MongoDB! :)");
         }
     }
 }
