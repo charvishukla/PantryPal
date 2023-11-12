24d9ba22f036c6012d307611482719b2d73a7d65
Earth34r <delaware.wade@gmail.com>
2023-11-12 14:46:59 -0800

    Add server class

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -31,12 +31,17 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URISyntaxException;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import com.sun.net.httpserver.*;
 
 public class Model {
     private AudioRecorder audioRecorder;
@@ -452,5 +457,28 @@ class Database{
         Bson filter = eq("Title", title);
         DeleteResult result = recipeCollection.deleteMany(filter);
         System.out.println(result);
+    }
+}
+
+class Server {
+    // initialize server port and hostname
+    private static final int SERVER_PORT = 8100;
+    private static final String SERVER_HOSTNAME = "localhost";
+
+    public static void start() throws IOException {
+        // create a thread pool to handle requests
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        // create a server
+        HttpServer server = HttpServer.create(
+        new InetSocketAddress(SERVER_HOSTNAME, SERVER_PORT),
+        0
+        );
+
+        // server.createContext("/", new RequestHandler(data));
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+
+        System.out.println("Server started on port " + SERVER_PORT);
     }
 }
