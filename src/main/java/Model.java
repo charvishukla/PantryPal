78a50d66c96f011e7d641b597f089ccfa78692c4
ImAmCoder <matthewhsu1128@gmail.com>
2023-11-12 14:23:03 -0800

    Complete speech text and audio recording

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -16,11 +16,21 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 import static com.mongodb.client.model.Updates.set;
-
+import javax.sound.sampled.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.json.JSONObject;
+import org.json.JSONException;
 import java.util.List;
 import java.util.ArrayList;
-
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -29,13 +39,13 @@ import java.net.URISyntaxException;
 import java.util.Map;
 
 public class Model {
-    private ChatGPT gpt;
     private String uri;
     private MongoDatabase database;
     private MongoCollection<Document> recipeCollection;
+    private AudioRecorder audioRecorder;
 
     public Model() {
-        gpt = new ChatGPT();
+        audioRecorder = new AudioRecorder();
         this.uri = "mongodb+srv://team13:CohNSwNGemiYmOOI@cluster0.1nejphw.mongodb.net/?retryWrites=true&w=majority";
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
@@ -80,7 +90,7 @@ public class Model {
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Error: " + ex.getMessage());
-        }       
+        }    
     }
 
     public void insert(List<String> recipeDetail) {
@@ -135,6 +145,29 @@ public class Model {
         System.out.println(result);
     }
 
+    public String audioToText() {
+            String message = "";
+    
+             try {
+                message = Whisper.audioToText(new File("recording.wav"));
+            } catch (IOException e) {
+                e.printStackTrace(); // or handle the exception in an appropriate way
+            } catch (URISyntaxException e) {
+                e.printStackTrace(); // or handle the exception in an appropriate way
+            }
+    
+            return message;
+    }
+
+    public void startRecording(){
+        audioRecorder.startRecording();
+    }
+
+    //To stop the recoding process. 
+    public void stopRecording(){
+        audioRecorder.stopRecording();
+    }
+
 }
 
 class ChatGPT {
@@ -142,15 +175,10 @@ class ChatGPT {
     private static final String API_KEY = "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";
     private static final String MODEL = "gpt-3.5-turbo";
 
-    private HttpClient client;
-
-    public ChatGPT() {
-        this.client = HttpClient.newHttpClient();
-    }
-
-    public String generate(String prompt) throws
+    public static String generate(String prompt) throws
     IOException, InterruptedException, URISyntaxException {
 
+        HttpClient client = HttpClient.newHttpClient();
         int maxTokens = 500;
 
         JSONObject requestBody = new JSONObject();
@@ -171,7 +199,7 @@ class ChatGPT {
         .build();
 
         // Send the request and receive the response
-        HttpResponse<String> response = this.client.send(request,
+        HttpResponse<String> response = client.send(request,
         HttpResponse.BodyHandlers.ofString());
         
         JSONObject responseJson = new JSONObject(response.body());
@@ -223,5 +251,199 @@ class Recipe {
 
     public void setMealType(String mealType) {
         this.mealType = mealType;
+    } 
+}
+
+class AudioRecorder {
+
+    private static AudioFormat audioFormat = new AudioFormat(44100, 16, 2, true, true);
+    TargetDataLine targetDataLine;
+
+    public void startRecording() {
+        Thread t = new Thread(
+            new Runnable(){
+                @Override
+                public void run(){
+                    try {
+                        // the format of the TargetDataLine
+                        DataLine.Info dataLineInfo = new DataLine.Info(
+                            TargetDataLine.class, audioFormat);
+
+                        // the TargetDataLine used to capture audio data from the microphone
+                        targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+                        targetDataLine.open(audioFormat);
+                        targetDataLine.start();
+
+                        // the AudioInputStream that will be used to write the audio data to a file
+                        AudioInputStream audioInputStream = new AudioInputStream(
+                                targetDataLine);
+
+                        // the file that will contain the audio data
+                        File audioFile = new File("recording.wav");
+                        AudioSystem.write(
+                                audioInputStream,
+                                AudioFileFormat.Type.WAVE,
+                                audioFile);
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        );
+
+        t.start();
+    }
+
+    public void stopRecording() {
+        targetDataLine.stop();
+        targetDataLine.close();
+    }
+    
+}
+
+class Whisper {
+    
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions";
+    private static final String TOKEN = "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";
+    private static final String MODEL = "whisper-1";
+
+    private static void writeParameterToOutputStream(
+        OutputStream outputStream,
+        String parameterName,
+        String parameterValue,
+        String boundary
+    ) 
+    throws IOException {
+        outputStream.write(("--" + boundary + "\r\n").getBytes());
+        outputStream.write(
+            (
+                "Content-Disposition: form-data; name=\"" + parameterName + "\"\r\n\r\n"
+            ).getBytes()
+        );
+        outputStream.write((parameterValue + "\r\n").getBytes());
+    }
+
+
+    // Helper method to write a file to the output stream in multipart form data format
+    private static void writeFileToOutputStream(
+        OutputStream outputStream,
+        File file,
+        String boundary
+    ) 
+    throws IOException {
+        outputStream.write(("--" + boundary + "\r\n").getBytes());
+        outputStream.write(
+        (
+            "Content-Disposition: form-data; name=\"file\"; filename=\"" +
+            file.getName() +
+            "\"\r\n"
+        ).getBytes()
+    );
+
+        outputStream.write(("Content-Type: audio/mpeg\r\n\r\n").getBytes());
+    
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+        fileInputStream.close();
+    }
+    
+
+    // Helper method to handle a successful response
+    private static String handleSuccessResponse(HttpURLConnection connection)
+    throws IOException, JSONException {
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(connection.getInputStream())
+        );
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        JSONObject responseJson = new JSONObject(response.toString());
+
+        String generatedText = responseJson.getString("text");
+
+        return generatedText;
+    }
+
+
+    // Helper method to handle an error response
+    private static void handleErrorResponse(HttpURLConnection connection)
+    throws IOException, JSONException {
+        BufferedReader errorReader = new BufferedReader(
+            new InputStreamReader(connection.getErrorStream())
+        );
+        String errorLine;
+        StringBuilder errorResponse = new StringBuilder();
+        while ((errorLine = errorReader.readLine()) != null) {
+            errorResponse.append(errorLine);
+        }
+        errorReader.close();
+        String errorResult = errorResponse.toString();
+        System.out.println("Error Result: " + errorResult);
+    }
+    
+    public static String audioToText(File file) throws IOException, URISyntaxException {
+
+        // Set up HTTP connection
+        URL url = new URI(API_ENDPOINT).toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+
+        // Set up request headers
+        String boundary = "Boundary-" + System.currentTimeMillis();
+        connection.setRequestProperty(
+    "Content-Type",
+        "multipart/form-data; boundary=" + boundary
+        );
+        connection.setRequestProperty("Authorization", "Bearer " + TOKEN);
+
+
+        // Set up output stream to write request body
+        OutputStream outputStream = connection.getOutputStream();
+
+
+        // Write model parameter to request body
+        writeParameterToOutputStream(outputStream, "model", MODEL, boundary);
+
+
+        // Write file parameter to request body
+        writeFileToOutputStream(outputStream, file, boundary);
+
+
+        // Write closing boundary to request body
+        outputStream.write(("\r\n--" + boundary + "--\r\n").getBytes());
+
+
+        // Flush and close output stream
+        outputStream.flush();
+        outputStream.close();
+
+
+        // Get response code
+        int responseCode = connection.getResponseCode();
+
+        String generatedText = "";
+
+        // Check response code and handle response accordingly
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            generatedText = handleSuccessResponse(connection);
+        } else {
+            handleErrorResponse(connection);
+        }
+
+        // Disconnect connection
+        connection.disconnect();
+
+        return generatedText;
     }
 }
