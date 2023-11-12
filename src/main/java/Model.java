a131c0abe6bbafd4436784242b0f37a3d72572bf
Earth34r <delaware.wade@gmail.com>
2023-11-11 18:49:27 -0800

    Add ChatGPT class

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -7,12 +7,25 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class Model{
-    public Model(){
+public class Model {
+    private ChatGPT gpt;
+
+    public Model() {
+        gpt = new ChatGPT();
         String uri = "mongodb+srv://team13:CohNSwNGemiYmOOI@cluster0.1nejphw.mongodb.net/?retryWrites=true&w=majority";
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             System.out.println("Successfully connected to MongoDB! :)");
         }
     }
+    
+    
+}
+
+class ChatGPT {
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
+    private static final String API_KEY = "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";
+    private static final String MODEL = "gpt-3.5-turbo";
+
+
 }
