1d2c4913be0103028243120da147f26602f01ca5
Baljot Sekhon <bsekhon@ucsd.edu>
2023-11-12 21:52:47 -0800

    Add Modeltest

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -8,19 +8,27 @@ import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import java.io.File;
 
+// @ExtendWith(MockitoExtension.class)
 class ModelTest {
-
     @Mock
-    private Model model;
-    private ChatGPTAPI mockedChatGPT;
+    private AudioRecorder mockedAudioRecorder;
+    @Mock
+    private Database mockedDatabase;
+    @Mock
+    private Whisper mockedWhisper;
+    @Mock
+    private Model mockedModel;
+    @Mock 
+    private ChatGPT mockedChatGPT;
+
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        model = new Model();
-        mockedChatGPT = new ChatGPTAPI();
+        mockedModel = new Model();
+        mockedChatGPT = new ChatGPT();
     }
 
     @Test 
@@ -32,13 +40,16 @@ class ModelTest {
 
     @Test 
     void testGenerateWithValidIngredient2() throws IOException, InterruptedException, URISyntaxException {
-            String ingredient = "potato, chilli, cheese, olive oil"; 
+            String ingredient = "potato, chili, cheese, olive oil"; 
             String recipe = mockedChatGPT.generate(ingredient);
             assertTrue(recipe instanceof String, "The generated recipe should be of String type");
     }
 
-   
- 
-
-
+    @Test
+    void audioToText_ReturnsText_OnValidFile() throws IOException, URISyntaxException {
+        mockedWhisper.audioToText(any(File.class));
+        Mockito.verify(mockedWhisper).audioToText(any(File.class));
+        when(mockedWhisper.audioToText(any(File.class))).thenReturn("Sample text");
+        assertEquals("Sample text", mockedModel.audioToText());
+    }
 }
