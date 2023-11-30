db181bff0cb3e2573bdd756dbf9cb77f6891f7db
Earth34r <delaware.wade@gmail.com>
2023-11-30 14:09:07 -0800

    Fixed US7-1

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -30,117 +30,23 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import org.json.JSONObject;
 import org.json.JSONException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Map;
 
-import java.util.Scanner;
-import com.sun.net.httpserver.*;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
-public class Model implements HttpHandler {
+public class Model {
     private AudioRecorder audioRecorder;
     private Database db;
 
     public Model() {
-        try {
-            Server.start(this);
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
         this.audioRecorder = new AudioRecorder();
         this.db = new Database();
-    }
-
-    public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Not handled";
-        String method = httpExchange.getRequestMethod();
-
-        try {
-            if (method.equals("GET")) {
-                response = handleGet(httpExchange);
-            } else if (method.equals("POST")) {
-                response = handlePost(httpExchange);
-            } else if (method.equals("PUT")) {
-                response = handlePut(httpExchange);
-            } else if (method.equals("DELETE")) {
-                response = handleDelete(httpExchange);
-            } else {
-                throw new Exception("Invalid Request: " + method);
-            }
-        } catch (Exception e) {
-            System.out.println("An erroneous request");
-            response = e.toString();
-            e.printStackTrace();
-
-            //Sending back response to the client
-            httpExchange.sendResponseHeaders(200, response.length());
-            OutputStream outStream = httpExchange.getResponseBody();
-            outStream.write(response.getBytes());
-            outStream.close();
-        }
-    }
-
-    private String handleGet(HttpExchange httpExchange) throws IOException {
-        String response = "Invalid GET request";
-        URI uri = httpExchange.getRequestURI();
-        String query = uri.getRawQuery();
-        if (query != null) {
-            String value = query.substring(query.indexOf("=") + 1);
-            String[] args = value.split("&");
-            switch (args[0]) {
-                case "gpt" -> {
-                    response = "pass";// parseGPTResponse(args[1]);
-                }
-            }
-        }
-        return response;
-    }
-
-    private String handlePost(HttpExchange httpExchange) throws IOException {
-        InputStream inStream = httpExchange.getRequestBody();
-        Scanner scanner = new Scanner(inStream);
-        String postData = scanner.nextLine();
-        String language = postData.substring(
-            0,
-            postData.indexOf(",")
-        ), year = postData.substring(postData.indexOf(",") + 1);
-
-        String response = "Posted entry {" + language + ", " + year + "}";
-        System.out.println(response);
-        scanner.close();
-
-        return response;
-    }
-
-    private String handlePut(HttpExchange httpExchange) throws IOException {
-        String response = "Invalid GET request";
-        URI uri = httpExchange.getRequestURI();
-        String query = uri.getRawQuery();
-        if (query != null) {
-            String value = query.substring(query.indexOf("=") + 1);
-            String year = "a"; // Retrieve data from hashmap
-        }
-        return response;
-    }
-
-    private String handleDelete(HttpExchange httpExchange) throws IOException {
-        String response = "Invalid GET request";
-        URI uri = httpExchange.getRequestURI();
-        String query = uri.getRawQuery();
-        if (query != null) {
-            String value = query.substring(query.indexOf("=") + 1);
-        }
-        return response;
     }
 
     public String audioToText() {
