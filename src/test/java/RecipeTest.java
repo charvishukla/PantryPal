ed2b0f221e6f9892cc3aa7db7c58bd5d6ba28ea7
Baljot Sekhon <bsekhon@ucsd.edu>
2023-12-05 15:31:03 -0800

    fixed testRecipeTitle text and added US7 BDDs

--- src/test/java/RecipeTest.java
+++ src/test/java/RecipeTest.java
@@ -18,10 +18,25 @@
 //         MockitoAnnotations.openMocks(this);
 //     }
 
+<<<<<<< Updated upstream
 //     @Test
 //     void testRecipeTitle() {
 //         List<String> list = Arrays.asList("test");
 //     }
+=======
+    /*@Test
+    void testRecipeTitle() {
+        List<String> list = Arrays.asList("test");
+    }*/
+
+    @Test
+    void testRecipeTitle() {
+        List<String> list = Arrays.asList("Pancakes", "Buttermilk\n\n\nFlour");
+        String result = Helper._splitTitle(list);
+        String expected = "Pancakes\n";
+        assertEquals(result, expected);
+    }
+>>>>>>> Stashed changes
 
 //     @Test
 //     void testSplitIngredients() {
@@ -41,6 +56,7 @@
 //         assertEquals(result, expected);
 //     }
 
+<<<<<<< Updated upstream
 //     @Test
 //     void testMealTypeBDD() {
 //         List<String> mockListFromDatabase = Arrays.asList("Egg Fired Rice", "Three eggs\n\n\nA touch of oil\nSalt(2g)\n\nOvernight leftover rice", "Step1: Hot pan and Add Oil", "Step2: Fry eggs", "Step3: Add Rice when eggs are cooked", "lunch");
@@ -49,3 +65,41 @@
 //         assertEquals(result, expected);
 //     }
 // }
+=======
+    @Test
+    void testMealTypeBDD() {
+        List<String> mockListFromDatabase = Arrays.asList("Egg Fired Rice", "Three eggs\n\n\nA touch of oil\nSalt(2g)\n\nOvernight leftover rice", "Step1: Hot pan and Add Oil", "Step2: Fry eggs", "Step3: Add Rice when eggs are cooked", "lunch");
+        String result = Helper._splitMealType(mockListFromDatabase);
+        String expected = "lunch";
+        assertEquals(result, expected);
+    }
+
+    /*
+     * BDD Scenario: Identify MealType as "breakfast" when retrieving breakfast recipe
+     * Given: Caitlin has a recipe for Breakfast
+     * When: Caitlin retrieves the recipe details
+     * Then: The meal type should be listed as "breakfast"
+     */
+    @Test
+    void testBreakfastBDD() {
+        List<String> mockListFromDatabase = Arrays.asList("Omelette", "Three eggs\n\n\nA touch of oil\nSalt(2g)\n\nWhisk eggs and pour into hot pan", "Step1: Heat pan", "Step2: Cook omelette", "Step3: Serve hot", "breakfast");
+        String result = Helper._splitMealType(mockListFromDatabase);
+        String expected = "breakfast";
+        assertEquals(result, expected);
+    }
+
+    /*
+     * BDD Scenario: Identify MealType as "dinner" when retrieving dinner recipe
+     * Given: Caitlin has a recipe for dinner
+     * When: Caitlin retrieves the recipe details
+     * Then: The meal type should be listed as "dinner"
+     */
+    @Test
+    void testDinnerBDD() {
+        List<String> mockListFromDatabase = Arrays.asList("Grilled Chicken", "Chicken breast\n\n\nOlive oil\nSalt and pepper\n\nMarinate chicken and grill", "Step1: Marinate chicken", "Step2: Grill chicken", "Step3: Serve hot", "dinner");
+        String result = Helper._splitMealType(mockListFromDatabase);
+        String expected = "dinner";
+        assertEquals(result, expected);
+    }
+}
+>>>>>>> Stashed changes
