da2a74debacf536d01213503c8545e1d0ef8b650
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-12-05 19:51:33 -0800

    Mocked Controller

--- src/test/java/MockController.java
+++ src/test/java/MockController.java
@@ -1,45 +1,64 @@
-// import java.util.Comparator;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-// import javafx.event.ActionEvent;
-
-// import java.time.LocalDateTime;
-
-
-// class MockController {
-//     public void handleFilterBoxClick(ActionEvent event) {
-//         String filter = "all"; // Mock filter value
-//         RecipeList recipeList = new RecipeList(); // Mock RecipeList
-//         List<RecipeCard> recipeCards = recipeList.getRecipeCards(); // Get mock recipe cards
-//         recipeList.deleteAllOnScene();
-    
-//         for (RecipeCard recipeCard : recipeCards) {
-//             if (filter.equals("all") || recipeCard.getMealType().equals(filter)) {
-//                 recipeList.addRecipeCardOnScene(recipeCard);
-//             }
-//         }
-//     }
+import javafx.event.ActionEvent;
 
 
-//     public void handleSortingBoxClick(ActionEvent event) {
-//         String sorting = "Alphabetically"; // Mock sorting value
-//         RecipeList recipeList = new RecipeList(); // Mock RecipeList
-//         List<RecipeCard> recipeCards = recipeList.getRecipeCards(); // Get mock recipe cards
-    
-//         // Sorting logic
-//         recipeCards.sort((card1, card2) -> {
-//             switch (sorting) {
-//                 case "Alphabetically":
-//                     return card1.getTitle().compareToIgnoreCase(card2.getTitle());
-//                 case "Newest to Oldest":
-//                     return card2.getTime().compareTo(card1.getTime());
-//                 default: // Oldest to Newest
-//                     return card1.getTime().compareTo(card2.getTime());
-//             }
-//         });
-    
-//         recipeList.deleteAllOnScene();
-//         recipeCards.forEach(recipeList::addRecipeCardOnScene);
-//     }
-    
-// }
+/**
+ * 
+ */
+public class MockController extends Controller {
+
+    private Boolean loggedout;
+    private List<RecipeCard> mockRecipeCards;
+
+    public MockController() {
+        super(null, null); 
+        mockRecipeCards = new ArrayList<>();
+        mockRecipeCards.add(new RecipeCard("Recipe 1", "MealType1", "2023-01-01T00:00:00Z"));
+        mockRecipeCards.add(new RecipeCard("Recipe 2", "MealType2", "2023-01-02T00:00:00Z"));
+        mockRecipeCards.add(new RecipeCard("Recipe 3", "MealType1", "2023-01-02T00:00:00Z"));
+        mockRecipeCards.add(new RecipeCard("Recipe 4", "MealType3", "2023-01-02T00:00:00Z"));
+        mockRecipeCards.add(new RecipeCard("Recipe 5", "MealType1", "2023-01-02T00:00:00Z"));
+    }
+
+    public List<RecipeCard> handleFilterBoxClick(String clickedFilter) {
+        List<RecipeCard> filteredCards = new ArrayList<>();
+        for (RecipeCard card : mockRecipeCards) {
+            if (card.getMealType().equals(clickedFilter)) {
+                filteredCards.add(card);
+            }
+        }
+        System.out.println("RecipeCards successfully filtered using " + clickedFilter + " property");
+        return filteredCards;
+    }
+
+    public List<RecipeCard> handleSortingBoxClick(String sortingMethod) {
+        System.out.println("Sorting RecipeCards based on " + sortingMethod);
+
+        if (sortingMethod.equals("Alphabetically")) {
+            Collections.sort(mockRecipeCards, (card1, card2) -> card1.getTitle().compareToIgnoreCase(card2.getTitle()));
+        } else if (sortingMethod.equals("Newest to Oldest")) {
+            Collections.sort(mockRecipeCards, (card1, card2) -> card2.getTime().compareTo(card1.getTime()));
+        } else {
+            Collections.sort(mockRecipeCards, (card1, card2) -> card1.getTime().compareTo(card2.getTime()));
+        }
+
+        return new ArrayList<>(mockRecipeCards); // Return the sorted list
+    }
+
+    public Boolean handleLogoutButtonClick() {
+        mockRecipeCards.clear();
+        System.out.println("Logged out and cleared RecipeCards");
+        return loggedout = true;
+    }
+
+
+    public boolean handleLoginButtonClick(String username, String password) {
+        System.out.println("Attempting login with username: " + username);
+        this.loggedout = loggedout; /// value does not update
+        return username.equals("validUser") && password.equals("validPass");
+    }
+
+}
