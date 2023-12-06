c61d6229d9f4563b77dbb6d691cb05ef74a4e2fb
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-12-05 21:05:21 -0800

    Mock controller implementation

--- src/test/java/MockController.java
+++ src/test/java/MockController.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javafx.event.ActionEvent;
 
 
 /**
- * 
+ * MockController is a mock implementation of the Controller class.
+ * It is used for unit testing the Controller's functionality without
+ * needing actual implementations of dependent classes and UI elements.
  */
 public class MockController extends Controller {
 
     private Boolean loggedout;
     private List<RecipeCard> mockRecipeCards;
 
+    /**
+     * Constructor for MockController. Initializes mock recipe cards.
+     */
     public MockController() {
         super(null, null); 
         mockRecipeCards = new ArrayList<>();
@@ -23,6 +27,13 @@ public class MockController extends Controller {
         mockRecipeCards.add(new RecipeCard("Recipe 5", "MealType1", "2023-01-02T00:00:00Z"));
     }
 
+
+    /**
+     * Mock implementation of handleFilterBoxClick. Filters recipe cards based on the provided filter.
+     * 
+     * @param clickedFilter The filter to be applied on recipe cards.
+     * @return List of filtered RecipeCard objects.
+     */
     public List<RecipeCard> handleFilterBoxClick(String clickedFilter) {
         List<RecipeCard> filteredCards = new ArrayList<>();
         for (RecipeCard card : mockRecipeCards) {
@@ -34,6 +45,14 @@ public class MockController extends Controller {
         return filteredCards;
     }
 
+
+    /**
+     * Mock implementation of handleSortingBoxClick. Sorts recipe cards based on the provided sorting method.
+     * 
+     * @param sortingMethod The sorting method to be applied (e.g., "Alphabetically").
+     *                      This is the sorting method that the user actually selected in the real implementation
+     * @return List of sorted RecipeCard objects.
+     */
     public List<RecipeCard> handleSortingBoxClick(String sortingMethod) {
         System.out.println("Sorting RecipeCards based on " + sortingMethod);
 
@@ -48,6 +67,12 @@ public class MockController extends Controller {
         return new ArrayList<>(mockRecipeCards); // Return the sorted list
     }
 
+
+    /**
+     * Mock implementation of handleLogoutButtonClick. Clears the recipe cards and sets the logout status.
+     * 
+     * @return The logout status after the operation.
+     */
     public Boolean handleLogoutButtonClick() {
         mockRecipeCards.clear();
         System.out.println("Logged out and cleared RecipeCards");
@@ -55,10 +80,29 @@ public class MockController extends Controller {
     }
 
 
+     /**
+     * Mock implementation of handleLoginButtonClick. Simulates user login.
+     * 
+     * @param username The username for login.
+     * @param password The password for login.
+     * @return true if login is successful, false otherwise.
+     */
     public boolean handleLoginButtonClick(String username, String password) {
         System.out.println("Attempting login with username: " + username);
         this.loggedout = loggedout; /// value does not update
         return username.equals("validUser") && password.equals("validPass");
     }
+
+
+        // Getters for unit testing
+        public Boolean getLoggedOutStatus() {
+            return loggedout;
+        }
+    
+        public List<RecipeCard> getMockRecipeCards() {
+            return mockRecipeCards;
+        }
+
+
 
 }
